798dcbc7c16c28d44a9be882c212ed4829977a52
mparje <126131806+mparje@users.noreply.github.com>
2023-05-27 04:46:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,7 @@ def generate_text(prompt):
     response = openai.Completion.create(
         engine='text-davinci-003',
         prompt=prompt,
-        max_tokens=100,
-        temperature=0.8,
+        max_tokens=500,
         n=1,
         stop=None,
         temperature=0.8
